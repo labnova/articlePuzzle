305af85973f4231daa22b4b95b72fcc47a2581c1
Labnova <progettofeb@gmail.com>
2020-03-23 23:02:03 +0100

    first message

---
+++ ArticlePuzzle/Article.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace ArticlePuzzle
+{
+    public class Article
+    {
+
+        private string nome;
+        private string siteArticle;
+        private string url;
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+        }
+
+        public string SiteArticle
+        {
+            get
+            {
+                return siteArticle;
+            }
+        }
+
+        public string Url
+        {
+            get
+            {
+                return url;
+            }
+        }
+
+        public Article()
+        {
+        }
+    }
+
+    //internal class per il ranking dell'article
+    internal class ArticleRanking
+    {
+        public int rankingInitial { get; set; }
+        public int impactOnRanking { get; set; }
+        public int rankingFinal { get; set; }
+
+        public void SetTotalRanking(string action)
+        {
+            switch (action)
+            {
+                case "like":
+                    rankingFinal = rankingInitial + impactOnRanking;
+                    break;
+                case "dislike":
+                    rankingFinal = rankingInitial - impactOnRanking;
+                    break;
+            }
+
+    
+        }
+    }
+}
---
+++ ArticlePuzzle/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Reflection;
+using ArticlePuzzle;
+
+namespace ArticlePuzzle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //utilizzare .GetMethod per prendere la Method Info
+            Type objInitialRanking = typeof(ArticleRanking);
+            object obj = Activator.CreateInstance(objInitialRanking);
+
+            //prendere le properties e settare l'initialRanking a 100
+            //TODO: IMPLEMENTARE MECCANISMO DI STORAGE PER RIPRENDERE IL RANKING INITIAL
+            foreach (PropertyInfo prop in objInitialRanking.GetProperties())
+            {
+                if (prop.Name == "rankingInitial")
+                {
+                    prop.SetValue(obj, 100);
+                }
+            }
+
+            MethodInfo mInfo = objInitialRanking.GetMethod("SetTotalRanking");
+
+            //TODO: PER ADESSO INSERIRE PER TESTARE I LIKE E I DISLIKE, DA AGGIUNGERE IN LOGICA QUANDO ARRIVANO GLI ARTICLE
+            setLikeForArticle(10, objInitialRanking, obj, mInfo);
+            setDislikeForArticle(5, objInitialRanking, obj, mInfo);
+
+
+        }
+
+        //method per riprendere l'objInitialRanking e settare il grado di like preferito
+        public static void setLikeForArticle(int like, Type objInitialRanking, object obj, MethodInfo mInfo)
+        {
+            foreach (PropertyInfo prop in objInitialRanking.GetProperties())
+            {
+                if (prop.Name == "impactOnRanking")
+                {
+                    prop.SetValue(obj, like);
+                }
+            }
+
+            //FIXME: METTERE LE EXCEPTION SU mInfo
+            mInfo.Invoke(obj, new String[] { "like" });
+        }
+
+
+        //method per riprendere l'objInitialRanking e togliere il grado di like preferito
+        public static void setDislikeForArticle(int dislike, Type objInitialRanking, object obj, MethodInfo mInfo)
+        {
+            foreach (PropertyInfo prop in objInitialRanking.GetProperties())
+            {
+                if (prop.Name == "impactOnRanking")
+                {
+                    prop.SetValue(obj, dislike);
+                }
+            }
+
+            mInfo.Invoke(obj, new String[] { "dislike" });
+
+        }
+    }
+}
